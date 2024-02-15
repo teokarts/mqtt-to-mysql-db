0e2090519f48b563d550cdb3aaa517dc5e52e74f
Thodoris <tkarts@gmail.com>
2024-02-15 09:14:50 +0200

    MInor Changes

--- index.js
+++ index.js
@@ -237,6 +237,6 @@ app.post("/createTable", (req, res) => {
 
 //CREATE TABLE END
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+app.listen(8080, () => {
+	console.log("Server is running on port 8080");
 });
